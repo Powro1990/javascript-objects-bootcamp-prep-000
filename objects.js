ea5499805a267b56f0436bf0393dabddd13ae587
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2018-06-04 16:55:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
 playlist[artistName] = songTitle
 return playlist
 }
+<<<<<<< HEAD
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName] 
 }
+=======
+function 
+>>>>>>> 27175666da34345443877002a2feb56e70188abc
